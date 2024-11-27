1c21ab922b1d74c0453908e9f58370e6b46297f4
Jooyoung Chung <jooyoungchung027@gmail.com>
2024-11-27 18:15:53 -0500

    testing 1 changes lp#14

--- testing1.py
+++ testing1.py
@@ -1,3 +1,6 @@
+"""
+looks like some leetcode problem
+
 def maximumWealth(self, accounts):
         wealth = []
         sum = 0
@@ -9,3 +12,29 @@ def maximumWealth(self, accounts):
         for i in range(len(wealth)):
             richest = max(richest, wealth[i])
         print(richest)
+"""
+
+
+"""
+testing floor division
+
+revX = 897
+x = 89798
+y = 3
+print(1 // 10)
+"""
+
+
+"""
+testing logic ??? idk 
+
+s = "examples243"
+for i in range(len(s) - 1):
+    print(s[i])
+"""
+
+
+"""testing for lp(leetcode problem)#14"""
+strs = ["apple", "banana", "honeydew"]
+for i in range(len(strs)):
+    
